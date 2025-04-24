2d1a4f9c678f491814031e632ee87d743660eb3b
Groogg <38508755+Groogg@users.noreply.github.com>
2025-04-24 10:28:25 -0400

    added highlight on yank

--- lua/groogg/init.lua
+++ lua/groogg/init.lua
@@ -3,3 +3,15 @@ vim.g.mapleader = " "
 require("groogg.lazy_init")
 require("groogg.remap")
 require("groogg.set")
+
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
