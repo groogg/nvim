66423139d287c4ea8bca9bc3931270f32c42a065
Groogg <38508755+Groogg@users.noreply.github.com>
2025-04-24 10:38:54 -0400

    fix highlight on yank

--- lua/groogg/init.lua
+++ lua/groogg/init.lua
@@ -5,8 +5,8 @@ require("groogg.remap")
 require("groogg.set")
 
 
-autocmd('TextYankPost', {
-    group = yank_group,
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('HighlightYank', {}),
     pattern = '*',
     callback = function()
         vim.highlight.on_yank({
